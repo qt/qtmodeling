3bae64fdb2ae9986b114ef4051ac682ad67daff0
Sandro S. Andrade <sandroandrade@kde.org>
2012-09-13 22:28:08 +0000

    Fix errors in documentation generation
    - Removal of local qdocconf. Documentation is generated in conjunction
    with Qt.
    - Fix incorrect handling of namespaces by qdoc.
    - Add template class.
    
    Change-Id: Ib1191c95256887479fb3a40d64ef712492c6aae2
    Reviewed-by: Sandro S. Andrade <sandroandrade@kde.org>

---
+++ src/mof/qtmofglobal.h
@@ -0,0 +1,73 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Sandro S. Andrade <sandroandrade@kde.org>
+** Contact: http://www.qt-project.org/
+**
+** This file is part of the QtLocation module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+#ifndef QTMOFGLOBAL_H
+#define QTMOFGLOBAL_H
+
+#include <QtCore/qglobal.h>
+
+QT_BEGIN_HEADER
+
+#ifndef QT_STATIC
+#    if defined(QT_BUILD_MOF_LIB)
+#        define Q_MOF_EXPORT Q_DECL_EXPORT
+#    else
+#        define Q_MOF_EXPORT Q_DECL_IMPORT
+#    endif
+#else
+#    define Q_MOF_EXPORT
+#endif
+
+#if defined(QT_NAMESPACE)
+#    define QT_BEGIN_NAMESPACE_MOF namespace QT_NAMESPACE { namespace QtMof {
+#    define QT_END_NAMESPACE_MOF } }
+#    define QT_USE_NAMESPACE_MOF using namespace QT_NAMESPACE::QtMof;
+#    define QT_PREPEND_NAMESPACE_MOF(name) ::QT_NAMESPACE::QtMof::name
+#else
+#    define QT_BEGIN_NAMESPACE_MOF namespace QtMof {
+#    define QT_END_NAMESPACE_MOF }
+#    define QT_USE_NAMESPACE_MOF using namespace QtMof;
+#    define QT_PREPEND_NAMESPACE_MOF(name) ::QtMof::name
+#endif
+
+QT_END_HEADER
+
+#endif // QTMOFGLOBAL_H
+
