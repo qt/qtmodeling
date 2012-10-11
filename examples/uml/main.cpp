ee36a53198ff3e0afe4942ed7c9ff41e76925d8b
Sandro S. Andrade <sandroandrade@kde.org>
2012-10-11 23:58:15 +0000

    Improve QtUml example
    Change-Id: Ic653a1dcbcbf2be8eb4167f5f7405b458dfe7ef9
    Reviewed-by: Sandro S. Andrade <sandroandrade@kde.org>

--- examples/uml/main.cpp
+++ examples/uml/main.cpp
@@ -7,45 +7,68 @@
 
 #include <QtCore/QDebug>
 #include <QtCore/QScopedPointer>
+#include <QtCore/QMetaProperty>
+
+typedef const QSet<QtUml::QPackage *> QPackageList;
+typedef const QSet<QtUml::QStereotype *> QStereotypeList;
+typedef const QSet<QtUml::QType *> QTypeList;
+
+void checkProperties(QObject *object)
+{
+    const QMetaObject *metaObject = object->metaObject();
+    int propertyCount = metaObject->propertyCount();
+    for (int i = 0; i < propertyCount; ++i) {
+        qDebug() << "Property" << metaObject->property(i).name();
+    }
+}
 
 int main ()
 {
     QtUml::QModel *model= new QtUml::QModel;
     model->setName("MyModel");
+
     QtUml::QPackage *package = new QtUml::QPackage;
     package->setName("Package1");
-    model->addNestedPackage(package);
+
     QtUml::QPrimitiveType *primitiveType = new QtUml::QPrimitiveType;
     primitiveType->setName("String");
-    model->addOwnedType(primitiveType);
+
     QtUml::QEnumeration *enumeration = new QtUml::QEnumeration;
     enumeration->setName("DirectionKind");
     QtUml::QEnumerationLiteral *directionIn = new QtUml::QEnumerationLiteral;
     directionIn->setName("DirectionIn");
     enumeration->addOwnedLiteral(directionIn);
-    model->addOwnedType(enumeration);
+
     QtUml::QClass *class_ = new QtUml::QClass;
     class_->setName("Student");
     class_->setAbstract(false);
+
+    model->addOwnedType(primitiveType);
+    model->addOwnedType(enumeration);
+    model->addNestedPackage(package);
     package->addOwnedType(class_);
 
-    qDebug() << "package->ownedElements()->size():" << package->ownedElements()->size();
-    qDebug() << "package->members()->size():" << package->members()->size();
-    qDebug() << "package->ownedMembers()->size():" << package->ownedMembers()->size();
-    qDebug() << "package->ownedRules()->size():" << package->ownedRules()->size();
-    qDebug() << "package->packagedElements()->size():" << package->packagedElements()->size();
+    qDebug() << "package->ownedElements()->size():" << model->ownedElements()->size();
+    qDebug() << "package->members()->size():" << model->members()->size();
+    qDebug() << "package->ownedMembers()->size():" << model->ownedMembers()->size();
+    qDebug() << "package->ownedRules()->size():" << model->ownedRules()->size();
+    qDebug() << "package->packagedElements()->size():" << model->packagedElements()->size();
+    model->removePackagedElement(model->packagedElements()->values().last());
+    qDebug() << "package->ownedMembers()->size():" << model->ownedMembers()->size();
+    qDebug() << "package->packagedElements()->size():" << model->packagedElements()->size();
 
-    typedef const QSet<QtUml::QPackage *> QPackageList;
     QScopedPointer<QPackageList> nestedPackages (model->nestedPackages());
     qDebug() << "package->nestedPackages()->size():" << nestedPackages->size();
 
-    typedef const QSet<QtUml::QStereotype *> QStereotypeList;
-    QScopedPointer<QStereotypeList> ownedStereotypes (package->ownedStereotypes());
+    QScopedPointer<QStereotypeList> ownedStereotypes (model->ownedStereotypes());
     qDebug() << "package->ownedStereotypes()->size():" << ownedStereotypes->size();
 
-    typedef const QSet<QtUml::QType *> QTypeList;
-    QScopedPointer<QTypeList> ownedTypes (package->ownedTypes());
+    QScopedPointer<QTypeList> ownedTypes (model->ownedTypes());
     qDebug() << "package->ownedTypes()->size():" << ownedTypes->size();
+
+    qDebug() << "class_->qualifiedName:" << class_->qualifiedName();
+
+    checkProperties(model);
 
     delete model; // That will delete all owned elements
 
