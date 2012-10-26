c16f314870b0be8ef5e14fe70a2e30726d7c5f91
Sandro S. Andrade <sandroandrade@kde.org>
2012-10-26 13:54:36 +0000

    Add initial QtUml editor implementation
    Change-Id: I3ef6def8911614e50458f672d2a2b14b782c0a35

---
+++ examples/uml/editor/mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtWidgets/QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class QTreeWidgetItem;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private Q_SLOTS:
+    void on_trvModel_currentItemChanged(QTreeWidgetItem *current, QTreeWidgetItem *previous);
+    
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
