c3497f71e9eb6d961d354e02e42b280546f448bb
Sandro S. Andrade <sandroandrade@kde.org>
2013-01-08 20:55:43 -0300

    Add initial QXmiReader implementation
    Change-Id: If2242a3740257fb78ae1cf4629bcae808728e255

---
+++ src/mof/qxmireader.cpp
@@ -0,0 +1,114 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Sandro S. Andrade <sandroandrade@kde.org>
+** Contact: http://www.qt-project.org/
+**
+** This file is part of the QtMof module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qxmireader.h"
+#include "qxmireader_p.h"
+
+#include <QtCore/QIODevice>
+#include <QtCore/QRegularExpression>
+#include <QtCore/QDebug>
+
+#include <QtWrappedObjects/QWrappedObject>
+#include <QtWrappedObjects/QMetaWrappedObject>
+#include <QtWrappedObjects/QMetaPropertyInfo>
+#include <QtWrappedObjects/QtWrappedObjectsEnumerations>
+
+using QtWrappedObjects::QMetaWrappedObject;
+using QtWrappedObjects::QMetaPropertyInfo;
+
+#include <QtMof>
+using namespace QtMof;
+
+QT_BEGIN_NAMESPACE_QTMOF
+
+QXmiReaderPrivate::QXmiReaderPrivate()
+{
+}
+
+QXmiReaderPrivate::~QXmiReaderPrivate()
+{
+}
+
+QXmiReader::QXmiReader(QObject *parent) :
+    QObject(*new QXmiReaderPrivate, parent)
+{
+}
+
+QXmiReader::~QXmiReader()
+{
+}
+
+QWrappedObject *QXmiReader::readFile(QIODevice *device)
+{
+    Q_D(QXmiReader);
+
+    d->reader.setDevice(device);
+    QWrappedObject *rootElement = 0;
+
+    while (!d->reader.atEnd()) {
+        d->reader.readNext();
+
+        if (d->reader.isStartElement()) {
+            QString elementName = d->reader.name().toString().prepend(QString::fromLatin1("Q")).append(QString::fromLatin1("*"));
+            if (d->reader.namespaceUri() == QString::fromLatin1("http://www.omg.org/spec/UML/20110701")) {
+                int type;
+                if ((type = QMetaType::type(elementName.toLatin1())) != QMetaType::UnknownType) {
+                    const QMetaObject *metaObject = QMetaType::metaObjectForType(type);
+                    if (metaObject) {
+                        rootElement = dynamic_cast<QWrappedObject *>(metaObject->newInstance());
+                        if (rootElement)
+                            rootElement->setObjectName(d->reader.attributes().value(QString::fromLatin1("name")).toString());
+                        else qDebug() << "newInstance retornou null";
+                    }
+                    else qDebug() << "Nao encontrei o metaobject";
+                }
+                else qDebug() << "Nao encontrei metatype de" << elementName;
+            }
+        }
+    }
+
+    return rootElement;
+}
+
+#include "moc_qxmireader.cpp"
+
+QT_END_NAMESPACE_QTMOF
+
---
+++ src/mof/qxmireader.h
@@ -0,0 +1,84 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Sandro S. Andrade <sandroandrade@kde.org>
+** Contact: http://www.qt-project.org/
+**
+** This file is part of the QtMof module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia  LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+#ifndef QTMOF_QXMIREADER_H
+#define QTMOF_QXMIREADER_H
+
+#include <QtMof/QtMofGlobal>
+
+// Base class includes
+#include <QtCore/QObject>
+
+QT_BEGIN_HEADER
+
+class QIODevice;
+
+namespace QtWrappedObjects
+{
+    class QWrappedObject;
+};
+using QtWrappedObjects::QWrappedObject;
+
+QT_BEGIN_NAMESPACE_QTMOF
+
+QT_MODULE(QtMof)
+
+class QXmiReaderPrivate;
+
+class Q_MOF_EXPORT QXmiReader : public QObject
+{
+    Q_OBJECT
+
+    Q_DISABLE_COPY(QXmiReader)
+    Q_DECLARE_PRIVATE(QXmiReader)
+
+public:
+    explicit QXmiReader(QObject *parent = 0);
+    virtual ~QXmiReader();
+
+    QWrappedObject *readFile(QIODevice *device);
+};
+
+QT_END_NAMESPACE_QTMOF
+
+QT_END_HEADER
+
+#endif // QTMOF_QXMIREADER_H
+
