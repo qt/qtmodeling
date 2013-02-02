d4bd3e9b0158fc366a06b7cf3215dee6a4d5448e
Sandro S. Andrade <sandroandrade@kde.org>
2013-02-02 20:55:27 -0300

    Fix wrong forward decl of QMetaPropertyInfo
    Change-Id: I471a69d2dbf1f2702927b0bcf0382dddb2e60644
    Reviewed-by: Sandro S. Andrade <sandroandrade@kde.org>

--- src/wrappedobjects/qwrappedobjectpointer.cpp
+++ src/wrappedobjects/qwrappedobjectpointer.cpp
@@ -3,9 +3,9 @@
 ** Copyright (C) 2012 Sandro S. Andrade <sandroandrade@kde.org>
 ** Contact: http://www.qt-project.org/
 **
-** This file is part of the QtWrappedObjectsWidgets module of the Qt Toolkit.
+** This file is part of the QtWrappedObjects module of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:LGPL$
+** :LGPL
 ** GNU Lesser General Public License Usage
 ** This file may be used under the terms of the GNU Lesser General Public
 ** License version 2.1 as published by the Free Software Foundation and
@@ -35,50 +35,8 @@
 **
 **
 **
-** $QT_END_LICENSE$
+**
 **
 ****************************************************************************/
-#ifndef PROPERTYEDITOR_H
-#define PROPERTYEDITOR_H
+#include "qwrappedobjectpointer.h"
 
-#include <QtWrappedObjectsWidgets/QtWrappedObjectsWidgetsGlobal>
-#include <QtWidgets/QAbstractItemDelegate>
-
-QT_BEGIN_HEADER
-
-QT_BEGIN_NAMESPACE
-
-struct QMetaPropertyInfo;
-
-QT_MODULE(QtWrappedObjectsWidgets)
-
-class Q_WRAPPEDOBJECTSWIDGETS_EXPORT PropertyEditor : public QWidget
-{
-    Q_OBJECT
-    Q_PROPERTY(int value READ value WRITE setValue USER true)
-
-public:
-    explicit PropertyEditor(QWidget *widget, QMetaPropertyInfo *metaPropertyInfo, QWidget *parent = 0);
-
-    int value() const;
-
-public Q_SLOTS:
-    void setValue(int value);
-
-Q_SIGNALS:
-    void commitData(QWidget *editor);
-    void closeEditor(QWidget *editor, QAbstractItemDelegate::EndEditHint hint = QAbstractItemDelegate::NoHint);
-
-private Q_SLOTS:
-    void resetClicked();
-
-private:
-    QWidget *_widget;
-    QMetaPropertyInfo *_metaPropertyInfo;
-};
-
-QT_END_NAMESPACE
-
-QT_END_HEADER
-
-#endif // PROPERTYEDITOR_H
